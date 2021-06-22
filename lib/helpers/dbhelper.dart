440638973ae727a414ce82ce07714f175c1daa63
Abyan Fawwaz <abyanfawwaz77@gmail.com>
2021-06-23 03:22:31 +0700

    Penambahan tabel pada database

--- lib/helpers/dbhelper.dart
+++ lib/helpers/dbhelper.dart
@@ -5,6 +5,7 @@ import 'package:path_provider/path_provider.dart';
 
 // TODO: Make Model
 // TODO: Import model
+import 'package:zeatech/models/infomodel.dart';
 
 class DbHelper {
   static DbHelper _dbHelper;
@@ -13,27 +14,82 @@ class DbHelper {
 
   Future<Database> initDb() async {
     Directory directory = await getApplicationDocumentsDirectory();
-    String path = directory.path + 'item.db';
+    String path = directory.path + 'dompet.db';
 
-    var moneyDatabase = openDatabase(path, version: 1, onCreate: _createDb);
+    var dompetDatabase = openDatabase(path, version: 1, onCreate: _createDb);
 
-    return moneyDatabase;
+    return dompetDatabase;
   }
 
-  // Tabel untuk money
+  // Pembuatan tabel informasi dan transaksi
   void _createDb(Database db, int version) async {
     await db.execute('''
-      CREATE TABLE curency (
+      CREATE TABLE IF NOT EXISTS info (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        price INTEGER,
-        stok INTEGER,
-        kodebarang TEXT
-      )
+        int pemasukan,
+        int pengeluaran,
+      ),
+      CREATE TABLE IF NOT EXISTS transaksi (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        int nominal,
+        text jenis_transaksi,
+        text tanggal_transaksi,
+      ),
     ''');
   }
 
   // TODO: Make CRUD function
+  // Select databases
+  Future<List<Map<String, dynamic>>> select() async {
+    Database db = await this.initDb();
+    var mapList = await db.query('item', orderBy: 'name');
+    return mapList;
+  }
+
+// Create databases
+  Future<int> insert(Item object) async {
+    Database db = await this.initDb();
+    int count = await db.insert('item', object.toMap());
+    return count;
+  }
+
+// Update databases
+  Future<int> update(Item object) async {
+    Database db = await this.initDb();
+    int count = await db
+        .update('item', object.toMap(), where: 'id=?', whereArgs: [object.id]);
+    return count;
+  }
+
+// Delete databases
+  Future<int> delete(int id) async {
+    Database db = await this.initDb();
+    int count = await db.delete('item', where: 'id=?', whereArgs: [id]);
+    return count;
+  }
+
+  Future<List<Item>> getItemList() async {
+    var itemMapList = await select();
+    int count = itemMapList.length;
+    List<Item> itemList = List<Item>();
+    for (int i = 0; i < count; i++) {
+      itemList.add(Item.fromMap(itemMapList[i]));
+    }
+    return itemList;
+  }
+
+  factory DbHelper() {
+    if (_dbHelper == null) {
+      _dbHelper = DbHelper._createObject();
+    }
+    return _dbHelper;
+  }
+  Future<Database> get database async {
+    if (_database == null) {
+      _database = await initDb();
+    }
+    return _database;
+  }
 
   Future close() async => _database.close();
 }
