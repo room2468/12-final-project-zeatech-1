7d6c02193fbd9b92cac8af16605356428ca8d7d6
Andrisyarif02 <andrisyarif02@gmail.com>
2021-06-23 05:55:39 +0700

    inisialisasi firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import 'package:zeatech/layouts/home.dart';
 import 'package:zeatech/layouts/login.dart';
 // import 'package:zeatech/layouts/logincoba.dart';
 import 'package:zeatech/layouts/welcome.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
